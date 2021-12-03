18ab4901e3f6e8fd07c752d35755ed6a36e72e5a
ChoKyuWon <kyuwoncho18@gmail.com>
2021-12-03 16:54:01 +0900

    ChoKyuWon/Day3
    Signed-off-by: ChoKyuWon <kyuwoncho18@gmail.com>

--- 2021/ChoKyuWon/day3.py
+++ 2021/ChoKyuWon/day3.py
@@ -17,16 +17,62 @@ def part1(data):
                 count0 += 1
             else:
                 count1 += 1
-        if count1 > count0:
-            gamma_rate += (1<<bit_length - i - 1)
-        else:
-            epsilon_rate += (1<<bit_length - i - 1)
+        bit = 0 if count0>count1 else 1
+        gamma_rate += (bit<<bit_length - i - 1)
     
-    print(gamma_rate * epsilon_rate)
+    mask = (1 << bit_length) - 1
+    epsilon_rate = gamma_rate ^ mask
+    return gamma_rate * epsilon_rate
 
 
 def part2(data):
-    return None
+    oxy_rate = 0
+    co2_rate = 0
+    bit_length = len(data[0])
+    oxy_pruned_list = data
+    co2_pruned_list = data
+    for i in range(bit_length):
+        count0 = 0
+        count1 = 0
+        for j in oxy_pruned_list:
+            if j[i] == "0":
+                count0 += 1
+            else:
+                count1 += 1
+        bit = 0 if count0>count1 else 1
+
+        _oxy_pruned_list = []
+        for node in oxy_pruned_list:
+            if int(node[i]) == bit:
+                _oxy_pruned_list.append(node)
+        if len(_oxy_pruned_list) == 1:
+            oxy_rate += int(_oxy_pruned_list[0][i:], 2)
+            break
+        oxy_pruned_list = _oxy_pruned_list
+        oxy_rate += (bit<<bit_length - i - 1)
+
+    for i in range(bit_length):
+        count0 = 0
+        count1 = 0
+        for j in co2_pruned_list:
+            if j[i] == "0":
+                count0 += 1
+            else:
+                count1 += 1
+        bit = 0 if count0<=count1 else 1
+
+        _co2_pruned_list = []
+        for node in co2_pruned_list:
+            if int(node[i]) == bit:
+                _co2_pruned_list.append(node)
+        if len(_co2_pruned_list) == 1:
+            print("Break!", i, _co2_pruned_list[0])
+            co2_rate += int(_co2_pruned_list[0][i:], 2)
+            break
+        co2_pruned_list = _co2_pruned_list
+        co2_rate += (bit<<bit_length - i - 1)
+    print(oxy_rate, co2_rate)
+    return oxy_rate * co2_rate
 
 # data = """00100
 # 11110
