5081b9d07cc6086bf98f410dc4f57afa95c4a05f
kimjy0126 <kimjy0126@skku.edu>
2021-12-17 01:29:25 +0900

    Day 9: Smoke Basin

--- 2021/kimjy0126(Jongyoon Kim)/src/days/day09.rs
+++ 2021/kimjy0126(Jongyoon Kim)/src/days/day09.rs
@@ -1,5 +1,5 @@
 pub fn day09() {
-    let input: Vec<Vec<i32>> = std::fs::read_to_string("input/input09_small.txt")
+    let input: Vec<Vec<i32>> = std::fs::read_to_string("input/input09.txt")
         .unwrap()
         .lines()
         .map(|l| l.as_bytes().iter()
@@ -37,6 +37,8 @@ fn day09_part1(map: &Vec<Vec<Option<i32>>>) {
     let row = map.len();
     let col = map[0].len();
 
+    // Q. why is this closure not function?
+    // A. for capturing map, instead of giving as parameter
     let get_adjacent = |r: usize, c: usize| -> Vec<Option<i32>> {
         vec![map[r - 1][c], map[r][c + 1], map[r + 1][c], map[r][c - 1]]
     };
@@ -63,11 +65,13 @@ fn day09_part1(map: &Vec<Vec<Option<i32>>>) {
 }
 
 fn day09_part2(map: &Vec<Vec<Option<i32>>>) {
+    use std::collections::VecDeque;
+
     #[derive(Debug, Copy, Clone)]
     struct Position {
         row: usize,
         col: usize
-    };
+    }
 
     fn get_low_point(map: &Vec<Vec<Option<i32>>>) -> Vec<Position> {
         let row = map.len();
@@ -98,5 +102,44 @@ fn day09_part2(map: &Vec<Vec<Option<i32>>>) {
         low_point
     }
 
-    println!("{:?}", get_low_point(map));
+    // Q. why is this closure not function?
+    // A. for capturing map, instead of giving as parameter
+    let get_adjacent_pos = |p: &Position| -> Vec<Position> {
+        let (r, c) = (p.row, p.col);
+        vec![Position { row: r - 1, col: c }, Position { row: r, col: c + 1}, Position { row: r + 1, col: c }, Position { row: r, col: c - 1 }]
+    };
+
+    let low_point: Vec<Position> = get_low_point(map);
+    let num_of_basin = get_low_point(map).len();
+    let mut basin_area = vec![1; num_of_basin];
+
+    let row = map.len();
+    let col = map[0].len();
+    let mut map_basin: Vec<Vec<Option<i32>>> = vec![vec![None; col]; row];
+    for (i, p) in low_point.iter().enumerate() {
+        map_basin[p.row][p.col] = Some(i as i32);
+    }
+
+    let mut pos_q: VecDeque<Position> = VecDeque::from(low_point);
+    while !pos_q.is_empty() {
+        let target_p = pos_q.pop_front().unwrap();
+        let adj = get_adjacent_pos(&target_p);
+
+        for adj_p in &adj {
+            if map[adj_p.row][adj_p.col].is_some() {
+                if map[adj_p.row][adj_p.col].unwrap() < 9
+                        && map[adj_p.row][adj_p.col].unwrap() > map[target_p.row][target_p.col].unwrap()
+                        && map_basin[adj_p.row][adj_p.col].is_none() {
+                    map_basin[adj_p.row][adj_p.col] = map_basin[target_p.row][target_p.col];
+                    basin_area[map_basin[adj_p.row][adj_p.col].unwrap() as usize] += 1;
+                    pos_q.push_back(*adj_p);
+                }
+            }
+        }
+    }
+
+    basin_area.sort();
+    basin_area.reverse();
+
+    println!("Part Two: {}", basin_area[0] * basin_area[1] * basin_area[2]);
 }
