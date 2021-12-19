b66e8ef47e62e704e1786d0facac50fa0933d8ad
ChoKyuWon <kyuwoncho18@gmail.com>
2021-12-19 17:11:29 +0900

    ChoKyuWon/Day18 - implement Graph
    Signed-off-by: ChoKyuWon <kyuwoncho18@gmail.com>

--- 2021/ChoKyuWon/day18.py
+++ 2021/ChoKyuWon/day18.py
@@ -3,7 +3,9 @@ from math import floor, ceil
 # p = Puzzle(2021, 18)
 # input_str = p.input_data
 input_str = """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"""
+
+"""
 [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
 [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
 [7,[5,[[3,8],[1,4]]]]
@@ -27,6 +29,8 @@ def split_pair(s:str):
             index = i
     return (s[1:index], s[index + 1:-1])
 
+
+
 class Graph():
     def __init__(self, s:str, height:int, parent=None, sub_left=None, sub_right=None):
         if (sub_left != None) and (sub_left!= None):
@@ -34,7 +38,9 @@ class Graph():
             self.left.parent = self
             self.right = sub_right
             self.right.parent = self
-            self.h = 0
+            self.h = -1
+            self.inc_height()
+            self.parent = None
             return
         self.parent = parent
         self.h = height
@@ -44,6 +50,23 @@ class Graph():
         if len(right) == 1: self.right = int(right)
         else:self.right = Graph(right, height + 1, self)
     
+    def split_node(self, left:bool):
+        if left:
+            val = self.left
+            self.left = Graph("[{},{}]".format(floor(val/2), ceil(val/2)), self.h +1, self)
+            if self.left.h >= 4:
+                self.left.explode()
+        else:
+            val = self.right
+            self.right = Graph("[{},{}]".format(floor(val/2), ceil(val/2)), self.h +1, self)
+            if self.right.h >= 4:
+                self.right.explode()
+                
+    def inc_height(self):
+        self.h += 1
+        if isinstance(self.left, int) == False: self.left.inc_height()
+        if isinstance(self.right, int) == False: self.right.inc_height()
+
     def magnitute(self):
         if isinstance(self.left, int): left = self.left
         else: left = self.left.magnitute()
@@ -56,43 +79,84 @@ class Graph():
     def isPair(self):
         if isinstance(self.left, int) and isinstance(self.right, int): return True
         else: return False
-    def explode(self):
-        target = self
-        while target.isPair():
-            target = target.left
-        print("Find target!", target.left, target.right)
-        exit()
-        p = self.parent
-        pos = self
-        while pos is p.left:
-            pos = pos.parent
-            p = pos.parent
-
-        
-        p = self.parent
-        pos = self
-        if self.parent.left is self:
-            pass
-        else:
-            pass
     
+    def explode(self):
+        lazy_split = []
+
+        target = self
+        while target.isPair() == False:
+            target = target.left
+        
+        pos = target
+        flag = False
+        while True:
+            if pos is pos.parent.right: break
+            pos = pos.parent
+            if pos.parent == None:
+                flag = True
+                break
+        if flag == False:
+            pos = pos.parent
+            if isinstance(pos.left, int) == False:
+                pos = pos.left
+                while True:
+                    if isinstance(pos.right, int): break
+                    pos = pos.right
+                pos.right += target.left
+                if pos.right > 10:
+                    lazy_split.append((pos, False))
+            else:
+                pos.left += target.left
+                if pos.left > 10:
+                    lazy_split.append((pos, True))
+
+        pos = target
+        flag = False
+        while True:
+            if pos is pos.parent.left: break
+            pos = pos.parent
+            if pos.parent == None:
+                flag = True
+                break
+        if flag == False:
+            pos = pos.parent
+            if isinstance(pos.right, int) == False:
+                pos = pos.right
+                while True:
+                    if isinstance(pos.left, int): break
+                    pos = pos.left
+                pos.left += target.right
+                if pos.left > 10:
+                    lazy_split.append((pos, True))
+            else:
+                pos.right += target.right
+                if pos.right > 10:
+                    lazy_split.append((pos, False))
+        
+        if target is target.parent.left: target.parent.left = 0
+        else: target.parent.right = 0
+
+        for node, boolean in lazy_split:
+            node.split_node(boolean)
+
     def trim(self):
         if self.h >= 4:
             self.explode()
         
         if isinstance(self.left, int) == False:
-            self.left = self.left.trim()
+            self.left.trim()
         else:
             if self.left > 10:
-                self.left = Graph("[{},{}]".format(floor(self.left/2), ceil(self.left/2)), self.h +1, self)
+                self.split_node(True)
         
         if isinstance(self.right, int) == False:
-            self.right = self.right.trim()
+            self.right.trim()
         else:
             if self.right > 10:
-                self.right = Graph("[{},{}]".format(floor(self.right/2), ceil(self.right/2)), self.h +1, self)
+                self.split_node(False)
+
+
         
-        return self
 
     def __add__(self, g):
         new_graph = Graph(None, None, None, self, g)
@@ -107,10 +171,9 @@ class Graph():
         return s
 
 
-g = Graph("[[[[4,3],4],4],[7,[[8,4],9]]]", 0)
-g2 = Graph("[1,1]", 0)
-g3 = g + g2
-exit()
+g = Graph(data[0], 0)
 for i in range(1, len(data)):
     tmp = Graph(data[i], 0)
     g = g + tmp
+
+print(str(g))
